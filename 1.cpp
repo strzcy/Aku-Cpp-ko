900ef4c2e9c8ac9b1c4e2eabe00b167fdd4bdf52
strzcy <desipuspita46832@gmail.com>
2025-07-23 22:18:18 +0700

    1

---
+++ 1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+//comment
+/* multi
+line
+comment
+*/
+
+int main()
+{
+	int a;
+	std::cout << "halo kalian" << std::endl;
+	std::cin >> a;
+	std::cout << a << std::endl;
+	return 0;
+}
+
+// g++ dulu  kaya gini g++ 1.cpp1.
+// ./ klo udah ada aplikasi exmple ./1.exe
